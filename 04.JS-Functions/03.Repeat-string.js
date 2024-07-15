4768c91553bc5654de18b099f12d26a45831c7a8
Ивайло Пръмов <ivaylopr@gmail.com>
2024-07-15 12:25:58 +0300

    JS-Functions Lab

---
+++ 04.JS-Functions/03.Repeat-string.js
@@ -0,0 +1,6 @@
+function repeat(text,times){
+    let result=text.repeat(times);
+    console.log(result);
+}
+
+repeat("abc",3);
---
+++ 04.JS-Functions/04.Order.js
@@ -0,0 +1,12 @@
+function order(product,times){
+    let result;
+    if (product==="coffee") {
+        
+    } else if (product==="water") {
+        
+    } else if (product==="coke") {
+        
+    } else if (product==="snacks") {
+        
+    }
+}
