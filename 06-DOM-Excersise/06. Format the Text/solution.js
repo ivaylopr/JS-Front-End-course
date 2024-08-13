b3dd815f0d7aebaf505393eef33478c612920546
Ивайло Пръмов <ivaylopr@gmail.com>
2024-08-13 10:06:37 +0300

    add

--- 06-DOM-Excersise/06. Format the Text/solution.js
+++ 06-DOM-Excersise/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  const inputElement = document.getElementById('input');
+  const outputElement = document.getElementById('output');
+
+  function createParagraphElement(text) {
+    const pEl = document.createElement('p');
+    pEl.textContent = text;
+    return pEl;
+  }
+
+  const sentences = inputElement.value.split('.').filter(Boolean)
+    .map((x) => x.trim());
+
+  for (let i = 0; i < sentences.length; i +=3) {
+    const currentText=sentences.slice(i, i+3).join('. ').concat('.');
+    const currentParagraph = createParagraphElement(currentText);
+    outputElement.appendChild(currentParagraph);
+  }
+
+
 }
--- 06-DOM-Excersise/07. Hell's Kitchen/app.js
+++ 06-DOM-Excersise/07. Hell's Kitchen/app.js
@@ -1,8 +1,33 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   const inputTextArea = document.querySelector('#inputs textarea');
+   const outputEl = document.querySelector('#outputs p');
+   const workersEe = document.querySelector('#workers p');
+
+   function onClick() {
+
+      const restaurants = JSON.parse(inputTextArea.value)
+         .reduce((acc,data) => {
+            const[restaurantName, workersData]=data.split(' - ');
+            const workers=workersData.split(', ').map((workerData)=>{
+               const [name,salary]=workerData.split(' ');
+               
+               return {
+                  name,
+                  salary:Number(salary)
+               };
+            })
+            if (!acc.hasOwnProperty(restaurantName)) {
+               acc[restaurantName]={
+                  workers:[]
+               };
+            }
+            acc[restaurantName].workerData.push([...workers])
+            return acc;
+         }, {})
+
+
    }
-}
+} 
+ 
