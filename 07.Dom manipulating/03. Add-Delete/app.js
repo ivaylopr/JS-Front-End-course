cff67fbde89b721682fa91412b415938e4d930d6
Ивайло Пръмов <ivaylopr@gmail.com>
2024-08-14 09:47:29 +0300

    add

--- 07.Dom manipulating/03. Add-Delete/app.js
+++ 07.Dom manipulating/03. Add-Delete/app.js
@@ -1,3 +1,29 @@
 function addItem() {
-    //TODO...
+    // get element references
+    const itemElement = document.getElementById('items');
+    const newItemTextElement = document.getElementById('newItemText');
+
+    //create new element with textContent of the input
+    const liElement = document.createElement('li');
+    liElement.textContent=newItemTextElement.value;
+
+    //create button delite
+    const deleteButton = document.createElement('a');
+    deleteButton.textContent='[Delete]'
+    //deleteButton.setAttribute('href','#')
+    deleteButton.href='#';
+
+    //attach event to the button
+    deleteButton.addEventListener('click', (e)=>{
+        e.currentTarget.parentElement.remove();
+    })
+
+    //add to liElement
+    liElement.append(deleteButton);
+
+    //Attach to DOM
+    itemElement.appendChild(liElement);
+
+    //clean input
+    newItemTextElement.value='';
 }
