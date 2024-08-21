8fe9b223d040ada4ea66a80892459ace90f50ab3
Ивайло Пръмов <ivaylopr@gmail.com>
2024-08-22 00:34:31 +0300

    add

--- 09.HTTP-and-AJAX-Exercise-Resources/02.Messenger/app.js
+++ 09.HTTP-and-AJAX-Exercise-Resources/02.Messenger/app.js
@@ -1,5 +1,46 @@
 function attachEvents() {
-    // TODO:
+    // http://localhost:3030/jsonstore/messenger
+    const baseURL = 'http://localhost:3030/jsonstore/messenger';
+
+    async function customFetch(url, options) {
+        return fetch(url, options).then((res) => res.json());
+    }
+
+    const submitElement = document.getElementById('submit');
+    const refreshElement = document.getElementById('refresh');
+    const messageElement = document.getElementById('messages');
+
+    const [authorInputElement, contentInputElement] =
+        document.querySelectorAll("div#controls input[type='text']");
+
+    function createMessageHandler() {
+        customFetch(baseURL, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                author: authorInputElement.value,
+                content: contentInputElement.value,
+            },)
+        }).then(()=>{
+            authorInputElement.value="";
+            contentInputElement.value="";
+        })
+    }
+
+    function getAllMessages() {
+        customFetch(baseURL).then(appendMessages);
+    }
+
+    function appendMessages(messagesResponse) {
+        messageElement.textContent= Object.values(messagesResponse).map(({author, content})=>`${author}: ${content}`)
+        .join('\n');
+    }
+
+
+    submitElement.addEventListener('click', createMessageHandler);
+    refreshElement.addEventListener('click', getAllMessages);
 }
 
 attachEvents();
--- 09.HTTP-and-AJAX-Exercise-Resources/03.Phonebook/app.js
+++ 09.HTTP-and-AJAX-Exercise-Resources/03.Phonebook/app.js
@@ -1,5 +1,97 @@
 function attachEvents() {
-    // TODO:
+    const baseURL = 'http://localhost:3030/jsonstore/phonebook';
+
+    async function customFetch(url, options) {
+        try {
+            const data = await fetch(url, options).then((res) => res.json());
+            return { data, error: null }
+        } catch (error) {
+            return { data: null, error };
+        }
+    }
+    const phoneBookElement = document.getElementById('phonebook');
+    const loadButtonElement = document.getElementById('btnLoad');
+    const createButtonElement = document.getElementById('btnCreate');
+
+    const [personInputElement, phoneInputElement] = document.querySelectorAll('input');
+
+    async function fetchAllPosts() {
+        const { data, error } = await customFetch(baseURL);
+
+        if (error) {
+            return
+        }
+        const posts = Object.values(data)
+
+        appendAllContactsElements(posts);
+    }
+
+    async function deleteContact(contact) {
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+
+        }
+        const { data, error } = await customFetch(`${baseURL}/${contact._id}`, options);
+
+        if (error) {
+            return;
+        }
+
+        console.log(data);
+
+        fetchAllPosts();
+    }
+
+
+    function appendAllContactsElements(posts) {
+        phoneBookElement.innerHTML = "";
+
+        posts.forEach((contact) => {
+            const { person, phone } = contact;
+            const liElement = document.createElement('li');
+
+            liElement.textContent = `${person}: ${phone}`;
+
+            const deleteElementButton = document.createElement('button');
+
+            deleteElementButton.textContent = 'Delete';
+            deleteElementButton.addEventListener('click', () =>
+                deleteContact(contact));
+
+            liElement.appendChild(deleteElementButton);
+            phoneBookElement.appendChild(liElement);
+        })
+    }
+
+    async function createContact() {
+        const { value: phone } = phoneInputElement;
+        const { value: person } = personInputElement;
+
+        
+        const {error} = await customFetch(baseURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                phone,
+                person
+            })});
+        if (error) {
+            return
+        }
+
+        phoneInputElement.value='';
+        personInputElement.value='';
+
+        fetchAllPosts();
+        
+    }
+
+    loadButtonElement.addEventListener('click', fetchAllPosts);
+    createButtonElement.addEventListener('click', createContact);
 }
 
 attachEvents();
